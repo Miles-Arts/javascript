49055518ed8fa35474a15dd781490a3c33fb50d2
Milton Figueredo Miles <gratno@gmail.com>
2022-11-24 15:59:46 -0500

    HOla JS

---
+++ index.js
@@ -0,0 +1,14 @@
+var hola = "Hola Gente";
+
+console.log(hola);
+
+const objeto = { 
+    a: 3,
+    ar: [2, 2],
+    nombre: "Pedro",
+    objeto2: {
+        
+    }
+}
+
+console.log('Hola ', objeto);
